3eebc5e02ad578b8931b576dca3ef6ae5caf99ef
Usuario <mateomaurotandil@hotmail.com>
2025-03-07 17:54:29 -0300

    ultimos cambio

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from "../assets/logo.webp";
+import logoAzul from "../assets/logoAzul.webp"
 import "../styles/nav.css";
 import MenuIcon from "../components/MenuIcon";
 import CloseIcon from "../components/CloseIcon";
+import SeparadorMobile from "./SeparadorMobile";
 
 const Nav = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+
+    // Efecto para cambiar la clase en <body> cuando el menú se abre/cierra
+    useEffect(() => {
+        const contenedor = document.querySelector(".contenedor-principal");
+        if (menuOpen) {
+            contenedor.classList.add("menu-active");
+            contenedor.classList.add("menu-abierto");
+        } else {
+            contenedor.classList.remove("menu-active");
+            contenedor.classList.remove("menu-abierto");
+        }
+    }, [menuOpen]);
+
 
     return (
         <nav className="nav">
@@ -29,12 +44,25 @@ const Nav = () => {
 
             {/* Menú de navegación */}
             <ul className={`menu ${menuOpen ? "open" : ""}`}>
-                <li><a href="#growers">GROWERS</a></li>
-                <li><a href="#servicios">SERVICIOS</a></li>
-                <li><a href="#equipo">NUESTRO EQUIPO</a></li>
-                <li><a href="#galeria">GALERIA</a></li>
-                <li><a href="#faq">PREGUNTAS FRECUENTES</a></li>
-                <li><a href="#contacto">CONTACTO</a></li>
+                <p className={`${menuOpen ? "" : "desaparecer"}`}>Menu</p>
+                <SeparadorMobile className={`${menuOpen ? "" : "desaparecer"}`} />
+                <li><a href="#growers" onClick={() => setMenuOpen(false)}>GROWERS</a></li>
+                <li><a href="#servicios" onClick={() => setMenuOpen(false)}>SERVICIOS</a></li>
+                <li><a href="#equipo" onClick={() => setMenuOpen(false)}>NUESTRO EQUIPO</a></li>
+                <li><a href="#galeria" onClick={() => setMenuOpen(false)}>GALERIA</a></li>
+                <li><a href="#faq" onClick={() => setMenuOpen(false)}>PREGUNTAS FRECUENTES</a></li>
+                <li><a href="#contacto" onClick={() => setMenuOpen(false)}>CONTACTO</a></li>
+                <button className={`${menuOpen ? "" : "desaparecer"}`}>Cupos limitados ¡Anotate hoy!</button>
+                <SeparadorMobile className={`${menuOpen ? "" : "desaparecer"}`} />
+                <img className={`${menuOpen ? "" : "desaparecer"}`} src={logoAzul} alt="Logo" />
+                <button className="btn-close-icon" onClick={() => {
+                    setMenuOpen(!menuOpen);
+                    console.log(`Menu abierto: ${menuOpen}`); // Agrega este log
+                }}>
+                    <CloseIcon
+                        className={`${menuOpen ? "" : "desaparecer"}`}
+                    />
+                </button>
 
             </ul>
         </nav>
